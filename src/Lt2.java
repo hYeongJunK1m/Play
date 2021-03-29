79ff68e0941840c52589975f557da24a957c67b1
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-29 23:04:44 +0900

    feat :

--- src/Lt2.java
+++ src/Lt2.java
@@ -4,27 +4,32 @@ import java.util.Random;
 public class Lt2 {
     public static void main(String[] args) {
 
-        Random random =  new Random();
+        Random random = new Random();
 
-        int lotto [] = new int[6];
-        int check [] = new int[45];
+
         System.out.println("=======================");
-        for(int k = 'A'; k <= 'E'; k++) {
+
+        int[] num = new int[6];
+        for (int k = 'A'; k <= 'E'; k++) {
             System.out.println((char) k + " 자 동 ");
-            for(int i = 0; i<lotto.length; i++){
-                lotto[i] = (int)(Math.random()*45)+1;
-                for(int j = 0; j<i; j++){
-                    if(check[lotto[i]]==0){
-                        check[lotto[i]]=1;
-                    }
-                    else
+
+            for (int i = 0; i < num.length; i++) {
+                int temp = (int) (Math.random() * 45) + 1;
+                num[i] = temp;
+
+
+                for (int j = 0; j < i; j++) {
+                    if (num[j] == temp) {
                         i--;
+                        break;
+                    }
                 }
-
             }
 
+            for (int i = 0; i < num.length; i++) {
+                System.out.print(num[i] + " ");
             }
-            System.out.println("=======================");
+
         }
     }
-
+}
